15fb548e75dfa56a7780c0e81f4472a6cb72b4fc
wsz82 <wojteksz82@gmail.com>
2019-10-21 22:26:17 +0200

    Added cache

---
+++ src/main/java/io/github/similar_words_generator/web_app/MvcConfig.java
@@ -0,0 +1,23 @@
+package io.github.similar_words_generator.web_app;
+
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.CssLinkResourceTransformer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                .resourceChain(false)
+                .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
+                .addTransformer(new CssLinkResourceTransformer());
+    }
+}
