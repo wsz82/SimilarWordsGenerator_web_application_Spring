37d835af6944aa7400d22f62b67976091ef2912d
wsz82 <wojteksz82@gmail.com>
2019-11-05 20:43:38 +0100

    Real time correction of min and max word lengths

--- src/main/resources/static/javascript-input-correction.js
+++ src/main/resources/static/javascript-input-correction.js
@@ -1,4 +1,6 @@
 var wordsInputInit;
+var minWordLengthInputInit;
+var maxWordLengthInputInit;
 
 document.getElementById("words").addEventListener("focus", function() {
     wordsInputInit = parseInt(document.getElementById("words").value);
@@ -12,5 +14,43 @@ document.getElementById("words").addEventListener("change", function() {
 
     if (isNaN(wordsInput) || wordsInput < 1) {
         document.getElementById("words").value = wordsInputInit;
+    }
+});
+
+document.getElementById("minWordLength").addEventListener("focus", function() {
+    minWordLengthInputInit = document.getElementById("minWordLength").value;
+});
+
+document.getElementById("minWordLength").addEventListener("change", function() {
+    var minWordLength;
+
+    minWordLength = document.getElementById("minWordLength").value;
+    if (!(minWordLength == "default")) {
+        minWordLength = parseInt(minWordLength);
+    }
+
+    document.getElementById("minWordLength").value = minWordLength;
+
+    if ((isNaN(minWordLength) || minWordLength == "default") || minWordLength < 1) {
+        document.getElementById("minWordLength").value = minWordLengthInputInit;
+    }
+});
+
+document.getElementById("maxWordLength").addEventListener("focus", function() {
+    maxWordLengthInputInit = document.getElementById("maxWordLength").value;
+});
+
+document.getElementById("maxWordLength").addEventListener("change", function() {
+    var maxWordLength;
+
+    maxWordLength = document.getElementById("maxWordLength").value;
+    if (!(maxWordLength == "default")) {
+        maxWordLength = parseInt(maxWordLength);
+    }
+
+    document.getElementById("maxWordLength").value = maxWordLength;
+
+    if ((isNaN(maxWordLength) || maxWordLength == "default") || maxWordLength < 1) {
+        document.getElementById("maxWordLength").value = maxWordLengthInputInit;
     }
 });
